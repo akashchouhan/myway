d1bfa11100d4b2a4081ee1e85a125bae030626d4
AkasH <AkasH@VAIO>
2018-04-11 00:16:54 +0530

    m

---
+++ mywayjava/src/mywayjava/HelloWorld.java
@@ -0,0 +1,10 @@
+package mywayjava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		System.out.println("wellcome to hello world");
+	}
+
+}
